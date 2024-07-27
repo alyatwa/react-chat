8ba73f8473972973e14b32ada5309fa20a47c5e6
ali atwa <ali.atwa2040@gmail.com>
2024-07-27 23:08:27 +0300

    disconnectMe

--- src/routes/chat/components/header.tsx
+++ src/routes/chat/components/header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
             <>
               <Button
                 onClick={() => {
-                  ws.emit("disconnect");
+                  ws.emit("disconnectMe");
                   logout();
                 }}
                 variant="link"
