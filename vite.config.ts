99787a3fa6a5b1711674f1dca405b32652e0b250
ali atwa <ali.atwa2040@gmail.com>
2024-07-29 19:37:14 +0300

    esnext

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext", // or "es2019",
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
